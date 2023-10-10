116687b3b80934be1233baec4c5f7b9aff070797
AlejandroSosaG <alejandro.sosa@iesnervion.es>
2023-10-10 14:39:41 +0200

    Unidad 6

---
+++ Unidad6/Unidad6/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Android.Graphics.Fonts;
+
+namespace Unidad6
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Onclick(object sender, EventArgs e)
+        {
+            Button button = new Button
+            {
+                Text = "Boton3",
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.Center,
+                Background = Color.FromArgb("#1b6df2"),
+                HeightRequest = 70,
+                WidthRequest = 200,
+                FontFamily = FontFamily.Verdana,
+                FontSize = 16,
+                FontAttributes = FontAttributes.Bold,
+                BorderColor = Color.FromArgb("#fafa02"),
+                Margin = 30,
+            };
+        }
+    }
+}
